4dba690f7bd2d62e169b6e19f34140cda2410b75
Anton Akhmedzyanov <anton.akhmedzyanov@gmail.com>
2025-02-02 19:50:31 +0300

    Initial Commit

---
+++ TwoSquares/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoSquares
+//
+//  Created by Anton Akhmedzyanov on 02.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
